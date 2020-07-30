4affb60d59f508c2389f45e9f8775b010bfdb9fa
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-07-30 23:41:04 +0430

    CURRENT_USER reducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -3,8 +3,7 @@ const { CURRENT_USER } = require("../../constants/types");
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case CURRENT_USER:
-      console.log(state);
-      return state;
+      return action.user
 
     default:
       return state;
