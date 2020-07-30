015b4eb1bf86b25210646397802ca89a39737ec4
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-07-31 01:50:34 +0430

    toggle drop down

--- src/components/shopping-bag/shopping-cart.component.jsx
+++ src/components/shopping-bag/shopping-cart.component.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
-import  './shopping-cart.styles.scss'
+import "./shopping-cart.styles.scss";
+import { toggleDropdown } from "../../redux/actions";
 const ShoppingBag = (props) => {
   console.log(props.user);
+  console.log(props);
   return (
     <div
-      style={{ padding: "0px 5px", display: props.user ? "flex" : "none",width:40,height:40  }}
+      style={{
+        padding: "0px 5px",
+        display: props.user ? "flex" : "none",
+        width: 40,
+        height: 40,
+        cursor: "pointer",
+      }}
       className="shopping-cart"
+      onClick={() => props.toggleDropdownn()}
     >
       <img
-        style={{ color: "black", width: "50%",height:'50%' }}
+        style={{ color: "black", width: "50%", height: "50%" }}
         src={require("../../assets/icons/shopping-bag-3-line-basket.png")}
       />
       <span className="shopping-cart-counter"> 5 </span>
@@ -21,4 +30,9 @@ const mapStateToProps = (state) => {
     user: state.user,
   };
 };
-export default connect(mapStateToProps)(ShoppingBag);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleDropdownn: () => dispatch(toggleDropdown()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingBag);
