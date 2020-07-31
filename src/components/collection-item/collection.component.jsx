cd9e11bf6676346646d853ea04e1f5a16b850cee
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-01 00:58:28 +0430

    set add to cart reducer

--- src/components/collection-item/collection.component.jsx
+++ src/components/collection-item/collection.component.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import "./collection-item.styles.scss";
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+import { connect } from "react-redux";
+import { addToCart } from "../../redux/actions";
+const CollectionItem = ({ id, name, price, imageUrl, addToCart }) => {
+  var item = {
+    id,
+    name,
+    price,
+    imageUrl,
+  };
   return (
     <div className="collection-item">
-      <div className="image" style={{backgroundImage:`url(${imageUrl})`}} />
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="collection-footer">
         <div className="collection-name">{name}</div>
         <div className="collection-price">{price}</div>
+        <button
+          onClick={() => {
+            addToCart(item);
+          }}
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   );
 };
-
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: (item) => dispatch(addToCart(item)),
+  };
+};
+export default connect(null, mapDispatchToProps)(CollectionItem);
