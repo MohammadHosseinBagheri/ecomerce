eba38a80b07c5eb889197d3f03e1c1d6b68c285f
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-01 01:00:58 +0430

    add to cart type

--- src/constants/types.js
+++ src/constants/types.js
@@ -1,4 +1,4 @@
 export const CURRENT_USER = "CURRENT_USER";
 export const LOGOUT_USER = "LOGOUT_USER";
 export const TOGGLE_DROPDOWN = "TOGGLE_DROPDOWN";
-
+export const ADD_TO_CART = "ADD_TO_CART";
