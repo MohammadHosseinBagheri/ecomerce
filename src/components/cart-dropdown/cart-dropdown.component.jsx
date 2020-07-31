0d4c3d668a79b5017dbb4d577dcee4873cc1c4d5
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-01 00:57:41 +0430

    add cart dropdown for add item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
-const CartDropdown = ({ hidden }) => {
+import { connect } from "react-redux";
+import CartDropdownItems from "../cart-dropdown-item/cart-dropdown-item.component";
+const CartDropdown = ({ cartDropdownItems }) => {
+  console.log(cartDropdownItems);
   return (
-    <div
-      className="cart-dropdown"
-    >
-      <div className="cart-item"></div>
+    <div className="cart-dropdown">
+      <div className="cart-item">
+        {cartDropdownItems.map((item) => (
+          <CartDropdownItems key={item.id} {...item} />
+        ))}
+      </div>
       <button>CHCEK OUT</button>
     </div>
   );
 };
-
-export default CartDropdown;
+const mapStateToProps = (state) => {
+  return {
+    cartDropdownItems: state.cart.addedItems,
+  };
+};
+export default connect(mapStateToProps)(CartDropdown);
