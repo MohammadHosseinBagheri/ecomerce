43e2d0267dbe06d02e6103a7d2f34d356f52b5cb
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-01 01:01:30 +0430

    add to cart reducer

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,18 +1,24 @@
-const { TOGGLE_DROPDOWN } = require("../../constants/types");
-
+import addedToCart from "./cart/cart.utils";
+const { TOGGLE_DROPDOWN, ADD_TO_CART } = require("../../constants/types");
 const INITIAL_STATE = {
   hidden: true,
+  addedItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TOGGLE_DROPDOWN:
-      console.log(state);
+      // console.log(state);
       return {
         ...state,
         hidden: !state.hidden,
       };
-
+    case ADD_TO_CART:
+      return {
+        ...state,
+        // addedItems: [...state.addedItems, action.item],
+        addedItems: addedToCart(state.addedItems, action.item),
+      };
     default:
       return state;
   }
