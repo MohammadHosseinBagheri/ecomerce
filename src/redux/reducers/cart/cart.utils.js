11ccc3a87bd3824f4aa4677561d531baea608fba
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-02 04:06:27 +0430

    remove and decrease item

--- src/redux/reducers/cart/cart.utils.js
+++ src/redux/reducers/cart/cart.utils.js
@@ -1,4 +1,4 @@
-const addedToCart = (items, addedItem) => {
+export const addedToCart = (items, addedItem) => {
   const isExist = items.find((item) => item.id === addedItem.id);
   if (isExist) {
     return items.map((item) => {
@@ -10,4 +10,21 @@ const addedToCart = (items, addedItem) => {
   }
   return [...items, { ...addedItem, quantity: 1 }];
 };
-export default addedToCart;
+
+export const decreaseFromCart = (addedItems, selectedItemForDecrease) => {
+  const existItems = addedItems.find(
+    (item) => item.id === selectedItemForDecrease
+  );
+  if (existItems.quantity === 1) {
+    return addedItems.filter((item) => item.id != selectedItemForDecrease);
+  }
+  return addedItems.map((item) =>
+    item.id === selectedItemForDecrease
+      ? { ...item, quantity: item.quantity - 1 }
+      : item
+  );
+};
+
+export const removeSelectedItem = (state, id) => {
+  return state.filter((item) => item.id != id);
+};
