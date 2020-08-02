c314678c70b0d63ff21aa8170877044a73676363
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-03 03:07:46 +0430

    add directory reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import userReducer from "./user";
 import cartReducer from "./cart";
+import directoryReducer from "./directory";
 
 const reducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default reducer;
