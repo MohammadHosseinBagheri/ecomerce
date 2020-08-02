74b6182603d9815909a895ef82d2137e5a5369f0
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-03 03:07:17 +0430

    add directory data to reducer

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -1,56 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
-const Directory = () => {
-  const [data, setData] = useState([
-    {
-      id: "0",
-      title: "shoes",
-      subtitle: "adidas",
-      linkUrl: "hats",
-      size: "",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: "1",
-      title: "glasses",
-      subtitle: "police",
-      size: "",
-      linkUrl: "hats",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: "2",
-      title: "pans",
-      subtitle: "black white",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      linkUrl: "hats",
-      size: "",
-    },
-    {
-      id: "3",
-      title: "tshirt",
-      subtitle: "nike",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
-      linkUrl: "hats",
-    },
-    {
-      id: "4",
-      title: "hats",
-      subtitle: "filla",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-      linkUrl: "hats",
-    },
-  ]);
+import { connect } from "react-redux";
+const Directory = ({directory}) => {
   return (
     <div className="directory-menu">
-      {data.map(({ id, ...itemProps }) => (
+      {directory.map(({ id, ...itemProps }) => (
         <MenuItem key={id} {...itemProps} />
       ))}
     </div>
   );
 };
-
-export default Directory;
+const mapStateToProps=state=>{
+  return{
+    directory:state.directory
+  }
+}
+export default connect(mapStateToProps)(Directory);
